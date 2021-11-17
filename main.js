16de0e18069bb8be6996dde11849871445502351
RakibTest <rakibmedieinstitutet@gmail.com>
2021-11-17 10:43:24 +0100

    add values

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("hej")
+
+
+
+document
+.querySelector("button")
+.addEventListener("click", countBudget)
+
+function countBudget(e) {
+   e.preventDefault();
+
+   //console.log("it works")
+
+  const select=  document.querySelector("select");
+
+  console.log(select.value)
+
+const description = document.querySelector(".desc").value;
+
+console.log(description);
+const number = document.querySelector(".num").value;
+console.log(number)
+
+}
